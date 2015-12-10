11893c12cff6e5019c4b59533c2524bfbfc7911d
Sebastian Potasiak <sebastian.potasiak@gmail.com>
2015-12-10 13:47:08 +0100

    Aliases fixed

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -6,25 +6,34 @@
 # File:   ~/.config/zsh/aliases.zsh
 #
 
-# Vim-like
+# Vim-like {{{
+
 alias :q="exit"
 
-# File listing
+# }}}
+# File listing {{{
+
 alias ls="ls -B --group-directories-first --color=auto"
 alias ll="ls -l"
 alias la="ls -A"
 alias lla="la -l"
 alias lg="ls -a | grep -i"
 
-# Grep
+# }}}
+# Grep {{{
+
 alias grep="grep --color=auto"
 
-# Python
+# }}}
+# Python {{{
+
 alias py="python"
 alias py.unittest="python -m unittest"
 alias py.profile="python -m cProfile"
 
-# Virtualenv
+# }}}
+# Virtualenv {{{
+
 alias venv="workon"
 alias venv.exit="deactivate"
 
@@ -49,11 +58,20 @@ alias venv.proj.cd="cdproject"
 
 alias venv.wipe="wipeenv"
 
-# Tmux
+# }}}
+# Tmux {{{
+
 alias tmux.new="tmux new -s"
 alias tmux.attach="tmux attach -t"
 alias tmux.ls="tmux ls"
 alias tmux.kill="tmux kill-session -t"
 alias tmux.killall="tmux ls | grep : | cut -d. -f1 | awk '{print substr($1, 0, length($1)-1)}' | xargs kill"
 
-# vim:ft=zsh
+# }}}
+# Mixed {{{
+
+alias mp3dl="cd $HOME/music && youtube-dl --extract-audio --audio-format mp3 --no-playlist"
+
+# }}}
+
+# vim:ft=zsh:foldmethod=marker:foldlevel=0
